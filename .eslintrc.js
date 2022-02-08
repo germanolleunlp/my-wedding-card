b5fe8b4b242d54de5eb58e739bfafd64f9a1a5c7
germanolleunlp <germanolle@gmail.com>
2022-02-08 11:07:49 -0300

    added linter and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  plugins: ['react', 'prettier', 'jest'],
+  rules: {
+    camelcase: 'off',
+    'import/no-dynamic-require': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id']
+        }
+      }
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id']
+        }
+      }
+    ],
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'no-unreachable': 'warn',
+    'no-underscore-dangle': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off'
+  }
+};
