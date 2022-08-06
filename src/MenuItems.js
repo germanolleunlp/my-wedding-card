735e575679ba156a03a695700cc7b56cd0ed00c0
German <germanolle@gmail.com>
2022-08-06 18:24:13 -0300

    Update MenuItems.js

--- src/MenuItems.js
+++ src/MenuItems.js
@@ -5,7 +5,7 @@ const MENU_ITEMS = [
   { href: '/gifts', text: 'Regalos' },
   { href: '/driving', text: 'Como llegar' },
   { href: '/phones', text: 'Taxis/Remises' },
-  { href: '/housing', text: 'Hospejade' }
+  { href: '/housing', text: 'Hospedaje' }
 ];
 
 export default MENU_ITEMS;
